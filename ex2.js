ace48461bdaf97e3c2cebf320342a26846ef1e25
krishnazen27 <krishnazen27@gmail.com>
2026-05-06 23:37:21 +0100

    updated code with Eulers function
    updated code with Eulers function

--- ex2.js
+++ ex2.js
@@ -2,6 +2,14 @@
 //https://projecteuler.net/problem=1
 //ex2 should output the sum of all multiples of a or b less than n
 
-let euler1=()=>alert();
+function solveEuler1() {
+    let sum = 0;
+    for (let i = 1; i < 10; i++) {
+        if (i % 3 === 0 || i % 5 === 0) sum += i;
+    }
+    return sum;  //output will be captured in variable sum
+}
 
-let eulerCustom=()=>alert()
+let euler1=()=>alert(`sum of all multiples of 2 or 3 less than 10 is: ${solveEuler1()}`);
+
+let eulerCustom=()=>alert(`sum of all multiples of 2 or 3 less than 10 is: ${solveEuler1()}`)
