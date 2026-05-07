b16b983fbffe89f8d1eed0b3560814f504d6deea
krishnazen27 <krishnazen27@gmail.com>
2026-05-07 01:12:38 +0100

    Updated ex2 with custom function logic
    Updated ex2 with custom function logic

--- ex2.js
+++ ex2.js
@@ -5,11 +5,23 @@
 function solveEuler1() {
     let sum = 0;
     for (let i = 1; i < 10; i++) {
-        if (i % 3 === 0 || i % 5 === 0) sum += i;
+        if (i % 3 == 0 || i % 5 == 0) sum += i;
     }
-    return sum;  //output will be captured in variable sum
+    return sum;
 }
 
-let euler1=()=>alert(`sum of all multiples of 2 or 3 less than 10 is: ${solveEuler1()}`);
+let euler1=()=>alert(`sum of all multiples of 3 or 5 less than 10 is: ${solveEuler1()}`);
 
-let eulerCustom=()=>alert(`sum of all multiples of 2 or 3 less than 10 is: ${solveEuler1()}`)
+function eulerCustom() {
+    const a = Number(document.getElementById("a").value);
+    const b = Number(document.getElementById("b").value);
+    const n = Number(document.getElementById("n").value);
+
+    let sum = 0;
+    for (let i = 1; i < n; i++) {
+        if (i % a == 0 || i % b == 0) {
+            sum += i;
+        }
+    }
+    alert(`Sum of multiples of ${a} or ${b} below ${n} is: ${sum}`);
+}
