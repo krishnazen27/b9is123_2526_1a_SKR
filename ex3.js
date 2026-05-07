ff5dec24730d0e78d25a12d288c5833b60d1df3a
krishnazen27 <krishnazen27@gmail.com>
2026-05-07 03:17:28 +0100

    Updated code for exc3 task 1
    Updated code for exc3 task 1

--- ex3.js
+++ ex3.js
@@ -1,14 +1,24 @@
-let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
-let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
-let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
+//let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
+//let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
+//let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
 
-let eulerlist=()=>
+let listeuler=(a,b,l)=>
 {
-    a=2//can un-hardcode
-    b=3
-    l=[1,2,3,4,5,6,7,9,10,10,10]  //document.getElementById('l').value.split(" ").map((x=>parseInt(x)); // if l is space separated numbers
+        let sum = 0;
+
+    for (let i of l) {
+        if (i % a === 0 || i % b === 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+//    a=2//can un-hardcode
+//    b=3
+//    l=[1,2,3,4,5,6,7,9,10,10,10]  //document.getElementById('l').value.split(" ").map((x=>parseInt(x)); // if l is space separated numbers
     //call listEuler1 and alert. - compute the sum of multiples of a or b in list l
 }
+
 let euler2Lists=()=>
 {
     a=[2,3]//can un-hardcode
@@ -21,4 +31,21 @@ let euler2Lists1=()=>
     a=[2,3,5]//can un-hardcode
     l=[1,2,3,4,5,6,7,9,10,10,10]
     //call listEuler3 and alert.
+}
+
+function eulerlist() {
+
+    const a = Number(document.getElementById("a1").value);
+    const b = Number(document.getElementById("b1").value);
+    const l = document.getElementById("l").value
+                    .split(",")
+                    .map(x => Number(x.trim()));
+
+    if (!a || !b || l.some(isNaN)) {
+        alert("Please enter valid numbers for A, B, and list L");
+        return;
+    }
+
+    const result = listeuler(a, b, l);
+    alert(`Sum of multiples of ${a} or ${b} in list L is: ${result}`);
 }
